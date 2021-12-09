af3b1f91c43206a8fb5822f10be01391f160aef3
ikoide <ian.koide@protonmail.com>
2021-12-09 14:52:26 -0500

    scraper updates (stocktwits changed identifiers again)

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ class Bot(object):
   def scrapeStockTwits(self):
     URL = "https://stocktwits.com/symbol/BTC.X"
     self.driver.get(URL)
-    time.sleep(2)
-    self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[1]/div[2]")\
+    time.sleep(1)
+    self.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[1]/div[2]")\
       .click()
 
     sentimentContainer = self.driver.find_element_by_class_name("infinite-scroll-component")
@@ -35,23 +35,17 @@ class Bot(object):
       
       if len(sentimentContainer.find_elements_by_xpath("./div")) > sentimentCount:
         for i in range(len(sentimentContainer.find_elements_by_xpath("./div")) - sentimentCount):
-          author = sentiments[i].find_element_by_xpath(f"/html/body/div[2]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[2]/div[3]/div/div[{ i+1 }]/div/div/article/div/div[2]/div[1]/span[1]/span/a/span").text
-          content = sentiments[i].find_element_by_xpath(f"/html/body/div[2]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[2]/div[3]/div/div[{ i+1 }]/div/div/article/div/div[2]/div[2]/div/div/div").text
+          author = sentiments[i].find_element_by_xpath(f"/html/body/div[3]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[2]/div[3]/div/div[{ i+1 }]/div/div/article/div/div[2]/div[1]/span[1]/span/a/span").text
+          content = sentiments[i].find_element_by_xpath(f"/html/body/div[3]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[2]/div[3]/div/div[{ i+1 }]/div/div/article/div/div[2]/div[2]/div/div/div").text
 
           try:
-            movement = sentiments[i].find_element_by_xpath(f"/html/body/div[2]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[2]/div[3]/div/div[{ i+1 }]/div/div/article/div/div[2]/div[1]/span[1]/span/a").text
-            movementValue = 0
-
-            if movement == "Bullish":
-              movementValue = 1
-            if movement == "Bearish":
-              movementValue = -1
+            movement = sentiments[i].find_element_by_xpath(f"/html/body/div[3]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div[2]/div[3]/div/div[{ i+1 }]/div/div/article/div/div[2]/div[1]/span[2]/span").text
 
             r = requests.post(API_URL, data = {
               "content": content,
               "author": author,
               "source": URL,
-              "sentiment": movementValue
+              "excSentiment": movement.lower()
             })
 
           except NoSuchElementException:
